fc9dc54ac3845073905ca4c4bf1765f1621830b7
1F10240227 <s1f102402271@iniad.org>
2025-07-17 12:03:45 +0900

    login機能の追加

--- todo/views.py
+++ todo/views.py
@@ -4,27 +4,55 @@ from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
-
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import check_password
 # Create your views here.
+
+
+def u_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            context = {
+        "error": 'パスワードまたはIDが間違っています',
+    }
+            return render(request,'todo/login.html',context)
+
+    return render(request, 'todo/login.html')
+
+@login_required
 def index(request):
     if request.method == 'POST':
         task = Task(title=request.POST['title'],
-                    due_at=make_aware(parse_datetime(request.POST['due_at'])))
+                    due_at=make_aware(parse_datetime(request.POST['due_at'])),user=request.user)
         task.save()
 
+    tasks = Task.objects.filter(user=request.user)
+
     if request.GET.get('order') == 'due':
-        tasks = Task.objects.order_by('due_at')
+        tasks = tasks.order_by('due_at')
     else:
-        tasks = Task.objects.order_by('-posted_at')
+        tasks = tasks.order_by('-posted_at')
 
     context = {
         'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
 
+@login_required
 def detail(request, task_id):
     try:
-        task = Task.objects.get(pk=task_id)
+        task = Task.objects.get(pk=task_id, user=request.user)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
 
@@ -33,9 +61,10 @@ def detail(request, task_id):
     }
     return render(request, 'todo/detail.html', context)
 
+@login_required
 def update(request, task_id):
     try:
-        task = Task.objects.get(pk=task_id)
+        task = Task.objects.get(pk=task_id, user=request.user)
     except Task.DoesNotExist:
         raise Http404('Task does not exist')
     if request.method == 'POST':
@@ -49,17 +78,19 @@ def update(request, task_id):
     }
     return render(request, 'todo/edit.html', context)
 
+@login_required
 def delete(request, task_id):
     try:
-        task = Task.objects.get(pk=task_id)
+        task = Task.objects.get(pk=task_id, user=request.user)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
     task.delete()
     return redirect(index)
 
+@login_required
 def close(request, task_id):
     try:
-        task = Task.objects.get(pk=task_id)
+        task = Task.objects.get(pk=task_id, user=request.user)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
     task.completed = True
