9ffb62304b66213c7a6adabb9a9085a0930d0f41
1F10240227 <s1f102402271@iniad.org>
2025-07-17 11:15:18 +0900

    models.pyにユーザー情報を追加

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
     posted_at = models.DateTimeField(default=timezone.now)
